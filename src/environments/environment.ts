38b2a6cc516972d863c02205d25fdec67942329f
Mananjara <mananjara888@gmail.com>
2026-02-22 18:07:06 +0300

    update url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // https://back-mean.onrender.com/api/seed/initiate
-    apiUrl: 'http://back-mean.onrender.com/api'
+    apiUrl: 'https://back-mean.onrender.com/api'
 };
